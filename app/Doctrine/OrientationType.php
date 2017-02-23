8c1cd595338c7134e164001d2a93245fd690cd11
remi-san <remi.san@gmail.com>
2017-02-24 00:23:19 +0100

    refactor: null orientation doctrine fix

--- app/Doctrine/OrientationType.php
+++ app/Doctrine/OrientationType.php
@@ -21,6 +21,20 @@ class OrientationType extends StringType
     }
 
     /**
+     * Converts a value from its PHP representation to its database representation
+     * of this type.
+     *
+     * @param mixed            $value    The value to convert.
+     * @param AbstractPlatform $platform The currently used database platform.
+     *
+     * @return mixed The database representation of the value.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return (string) $value ? : null;
+    }
+
+    /**
      * @param mixed            $value
      * @param AbstractPlatform $platform
      *
