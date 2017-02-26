bb97740f3442d2ec8a4da1c896990f4eed7461cc
remi-san <remi.san@gmail.com>
2017-02-26 18:09:05 +0100

    style: bootstrap lists

--- web/index.php
+++ web/index.php
@@ -5,4 +5,4 @@ use BrasseursApplis\Arrows\App\ApplicationConfig;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new ApplicationBuilder(new ApplicationConfig(true)))->httpApplication()->run();
+(new ApplicationBuilder(new ApplicationConfig(false)))->httpApplication()->run();
